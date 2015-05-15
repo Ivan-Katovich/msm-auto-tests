d7fb407650f1c5dd80fd05c18fabc0bfd81d1d09
Ivan Katovich <Ivan_Katovich@epam.com>
2015-05-15 13:48:39 +0300

    added tabs

--- msm-automatisation/src/test/java/com/epam/fw/shop/Shop.java
+++ msm-automatisation/src/test/java/com/epam/fw/shop/Shop.java
@@ -92,26 +92,26 @@ public abstract class Shop {
 			log.error("http://"+gridURI+gridPort+"/wd/hub");
 			
 			switch (driverType) {
-			case "Firefox":
-				driver = new FirefoxDriver();
-				break;
-			case "Chrome":
-				System.setProperty("webdriver.chrome.driver", "additional_libraries/chromedriver.exe");
-				driver = new ChromeDriver();
-				break;
-			case "RemoteFirefox":
-//				log.error(" ==== connect to http://"+SELENIUM_HOST_WORKST+":"+SELENIUM_PORT+"/wd/hub");
-				DesiredCapabilities firefoxCapability = DesiredCapabilities.firefox();
-				driver = new RemoteWebDriver(gridHost, firefoxCapability);
-				break;
-			case "RemoteChrome":
-//				System.setProperty("webdriver.chrome.driver", "additional_libraries/chromedriver.exe");;
-				log.error(" ==== connect to http://"+SELENIUM_HOST_LOCAL+":"+SELENIUM_PORT+"/wd/hub");
-				DesiredCapabilities chromeCapability = DesiredCapabilities.chrome();
-				driver = new RemoteWebDriver(gridHost, chromeCapability);
-				break;
-			default:
-				driver = new FirefoxDriver();
+				case "Firefox":
+					driver = new FirefoxDriver();
+					break;
+				case "Chrome":
+					System.setProperty("webdriver.chrome.driver", "additional_libraries/chromedriver.exe");
+					driver = new ChromeDriver();
+					break;
+				case "RemoteFirefox":
+//					log.error(" ==== connect to http://"+SELENIUM_HOST_WORKST+":"+SELENIUM_PORT+"/wd/hub");
+					DesiredCapabilities firefoxCapability = DesiredCapabilities.firefox();
+					driver = new RemoteWebDriver(gridHost, firefoxCapability);
+					break;
+				case "RemoteChrome":
+//					System.setProperty("webdriver.chrome.driver", "additional_libraries/chromedriver.exe");;
+					log.error(" ==== connect to http://"+SELENIUM_HOST_LOCAL+":"+SELENIUM_PORT+"/wd/hub");
+					DesiredCapabilities chromeCapability = DesiredCapabilities.chrome();
+					driver = new RemoteWebDriver(gridHost, chromeCapability);
+					break;
+				default:
+					driver = new FirefoxDriver();
 			}
 		} catch (Exception e) {
 			options.setErrorMessage("Can't setup " + driverType + " because " + e.getMessage());
