6439cab945d4e6d71e28134c72f4e164d2733693
Ivan Katovich <Ivan_Katovich@epam.com>
2015-05-18 14:47:45 +0300

    remove sikuli ability

--- msm-automatisation/src/test/java/com/epam/fw/shop/ImageShop.java
+++ msm-automatisation/src/test/java/com/epam/fw/shop/ImageShop.java
@@ -2,9 +2,9 @@ package com.epam.fw.shop;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
-import org.sikuli.script.FindFailed;
-import org.sikuli.script.Pattern;
-import org.sikuli.script.Screen;
+//import org.sikuli.script.FindFailed;
+//import org.sikuli.script.Pattern;
+//import org.sikuli.script.Screen;
 
 import com.epam.fw.object.MyImage;
 import com.epam.fw.services.ImageServices;
@@ -16,17 +16,17 @@ public class ImageShop extends Shop {
 	
 	private static final Logger log = Logger.getLogger(ImageShop.class);
 	
-	public static void assertImageOnPage(MyImage image) {
-		log.info("enter to function assertImageOnPage " + image.getName() + "'");
-		options.setDriver(driver);
-		options.setMyImage(image);
-		if (ImageServices.waitImageIsVisible(options)) {
-			log.info("image '" + image.getName() + "' was found ");
-		} else {
-			log.error("can't find image " + image.getName() + "'");
-			options.setErrorMessage("can't find image " + image.getName() + "'");
-			MultiServices.errorShutdown(options);
-		}
-	}
+//	public static void assertImageOnPage(MyImage image) {
+//		log.info("enter to function assertImageOnPage " + image.getName() + "'");
+//		options.setDriver(driver);
+//		options.setMyImage(image);
+//		if (ImageServices.waitImageIsVisible(options)) {
+//			log.info("image '" + image.getName() + "' was found ");
+//		} else {
+//			log.error("can't find image " + image.getName() + "'");
+//			options.setErrorMessage("can't find image " + image.getName() + "'");
+//			MultiServices.errorShutdown(options);
+//		}
+//	}
 
 }
